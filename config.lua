5031926658c519beec4bf2d1027b799c71b48903
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-03-10 12:42:35 -0700

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,36 @@
+Config = {}
+
+Config.NoMoney = "You need $5 to exchange gold"
+Config.NoInv = "You don't have any Gold Bars"
+Config.Shoptext = 'Press ~e~[E]~q~ to Exchange'
+Config.Selltext = 'You have exchanged '
+
+Config.BlipSprite = '-2029148647'
+Config.BlipName = 'Gold Exchange'
+Config.Ped = 'RE_GOLDPANNER_MALES_01'  -- Alter PED Here
+
+Config.Coords = {
+	vector3(1418.35,270.67,90.14)
+
+}
+
+Config.Blips = {
+
+	--{x = 1418.35, y = 270.67, z = 90.14},
+
+}
+
+Config.Chatarra = {
+	
+	{
+		['Text'] = "Exchange Gold Bars",
+		['SubText'] = "",
+		['Desc'] = "",
+		['Param'] = {
+			['ExchangeRate'] = 1,
+			['ExchangeCost'] = 5.0,
+			['Tipo'] = "goldbar",
+			['Mens'] = "Paid $5 to convert 1 Gold Bar"
+		}
+	}
+}
