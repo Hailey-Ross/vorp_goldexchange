ace8fefeff5aefa82bdf8ea0d6e69bafd8658755
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-03-11 08:02:54 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -11,27 +11,27 @@ AddEventHandler( 'gold:vender', function ( args )
     local User = VorpCore.getUser(source)
     local Character = User.getUsedCharacter
     local _src   = source
-    local _price = args['ExchangeRate']
-    local _exchange = args['ExchangeCost']
+    local _exchangerate = args['ExchangeRate']
+	local _exchangecost = args['ExchangeCost']
     local _model = args['Tipo']
-    local _mens  = args['Mens']
+	local _mens  = args['Mens']
 	
-    local inv_count = VorpInv.getItemCount(_src, _model)
+	local inv_count = VorpInv.getItemCount(_src, _model)
 	
     if inv_count < 1 then
         TriggerClientEvent( 'UI:NotificaVenta', _src, Config.NoInv )
         return
     end
 	
-	if Character.money < _exchange then
+	if Character.money < _exchangecost then
         TriggerClientEvent( 'UI:NotificaVenta', _src, Config.NoMoney )
         return
     end
 
 	VorpInv.subItem(_src, _model, 1)
-	Character.addCurrency(1, _price)
-	Character.removeCurrency(0, _exchange)
+	Character.addCurrency(1, _exchangerate)
+	Character.removeCurrency(0, _exchangecost)
 	
-	TriggerClientEvent( 'UI:NotificaVenta', _src, Config.Selltext .. _mens )
+	TriggerClientEvent( 'UI:NotificaVenta', _src, Config.Selltext .. _exchangecost .. Config.ExchangeText .. _exchangerate .. _mens )
 	
 end)
