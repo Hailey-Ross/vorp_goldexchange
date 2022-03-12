7748cb420031c9d748a020c81552d8be01022f74
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-03-12 09:40:52 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -6,32 +6,61 @@ end)
 
 VorpInv = exports.vorp_inventory:vorp_inventoryApi()
 
+-- Convert Gold Bars to Currency
 RegisterServerEvent('gold:vender')
 AddEventHandler( 'gold:vender', function ( args )
     local User = VorpCore.getUser(source)
     local Character = User.getUsedCharacter
     local _src   = source
-    local _exchangerate = args['ExchangeRate']
-    local _exchangecost = args['ExchangeCost']
-    local _model = args['Tipo']
-    local _mens  = args['Mens']
+    local _exchangerateto = args['ExchangeRateTo']
+	local _exchangecostto = args['ExchangeCostTo']
+    local _model = args['Item']
 	
-    local inv_count = VorpInv.getItemCount(_src, _model)
+	local inv_count = VorpInv.getItemCount(_src, _model)
 	
+    if Character.money < _exchangecostto then
+        TriggerClientEvent( 'UI:NotificaVenta', _src, Config.NoMoney )
+        return
+    end
+    
     if inv_count < 1 then
         TriggerClientEvent( 'UI:NotificaVenta', _src, Config.NoInv )
         return
     end
+
+	VorpInv.subItem(_src, _model, 1)
+	Character.addCurrency(1, _exchangerateto)
+	Character.removeCurrency(0, _exchangecostto)
 	
-    if Character.money < _exchangecost then
-        TriggerClientEvent( 'UI:NotificaVenta', _src, Config.NoMoney )
+	TriggerClientEvent( 'UI:NotificaVenta', _src, Config.Selltext .. _exchangecostto .. Config.ExchangeText .. _exchangerateto .. Config.ItemMessage )
+	
+end)
+
+-- Convert Currency back to Gold Bars
+RegisterServerEvent('gold:revender')
+AddEventHandler( 'gold:revender', function ( args )
+    local User = VorpCore.getUser(source)
+    local Character = User.getUsedCharacter
+    local _src   = source
+    local _exchangerateback = args['ExchangeBarsBack']
+	local _exchangecostback = args['ExchangeCurrencyCost']
+    local _model = args['Item']
+	
+	local inv_count = VorpInv.getItemCount(_src, _model)
+	
+    if Character.gold < _exchangecostback then
+        TriggerClientEvent( 'UI:NotificaVenta', _src, Config.NoGold )
+        return
+    end
+    
+    if inv_count > 99 then
+        TriggerClientEvent( 'UI:NotificaVenta', _src, Config.NoSpace )
         return
     end
 
-	VorpInv.subItem(_src, _model, 1)
-	Character.addCurrency(1, _exchangerate)
-	Character.removeCurrency(0, _exchangecost)
+	VorpInv.addItem(_src, _model, _exchangerateback)
+	Character.removeCurrency(1, _exchangecostback)
 	
-	TriggerClientEvent( 'UI:NotificaVenta', _src, Config.Selltext .. _exchangecost .. Config.ExchangeText .. _exchangerate .. _mens )
+	TriggerClientEvent( 'UI:NotificaVenta', _src, Config.SelltextBack .. _exchangecostback .. Config.ExchangeTextBack .. _exchangerateback .. Config.ItemMessage )
 	
 end)
